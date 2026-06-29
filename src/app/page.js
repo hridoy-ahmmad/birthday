d8bcf03e41a89a612f9ca917aa4ddb5f71b752bc
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-06-30 00:27:46 +0600

    reduce volume

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,11 @@ import { useEffect, useRef } from "react";
 import FlowOne from "./componenets/FlowOne";
 
 export default function Home() {
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.volume = 0.2;
-    }
-  }, []);
 
   return (
     <div>
-      <audio ref={audioRef} autoPlay loop>
-        <source src="/birthday2.mp3" type="audio/mpeg" />
+      <audio autoPlay loop>
+        <source src="birthday2.mp3" type="audio/mpeg" />
       </audio>
       <FlowOne />
     </div>
